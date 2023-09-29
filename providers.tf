1064eed0e1c961acba8604ebcc794c38bd20821d
Shanmugam37 <99902963+Shanmugam37@users.noreply.github.com>
2023-09-29 17:07:00 +0530

    Add files via upload

--- providers.tf
+++ providers.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6MOPLRVZA3K6GMVN"#"AKIASNXDJG5C3OGG3K4H"
-  secret_key = "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"#"N9s+YD9V3vCkMqitG7LgDG44Mzq/R5WBf4LsRYZh"
+  access_key = "AKIASNXDJG5C3OGG3K4H"                     #"AKIA6MOPLRVZA3K6GMVN"                    
+  secret_key = "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" #"6ETz8hvWhcUDNPiE+PudUyPo5hNk5G24oJWIfTgN" 
 }
